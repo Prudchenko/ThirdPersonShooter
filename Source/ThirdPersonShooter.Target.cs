e5c6ebbfac1f16703c25b86f56c61d07c4d96f10
Prudchenko <javaj@ukr.net>
2023-04-10 19:11:33 +0300

    Initial commit

---
+++ Source/ThirdPersonShooter.Target.cs
@@ -0,0 +1,15 @@
+// Third Person Shooter, All Rights Reserved
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class ThirdPersonShooterTarget : TargetRules
+{
+	public ThirdPersonShooterTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V2;
+
+		ExtraModuleNames.AddRange( new string[] { "ThirdPersonShooter" } );
+	}
+}
---
+++ Source/ThirdPersonShooter/Public/TPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Third Person Shooter, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONSHOOTER_API ATPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
